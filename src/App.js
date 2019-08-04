5be4d7e81860ea301fbd9b24fa53f63911f7ba48
Da1man <chaos4ever@mail.ru>
2019-08-04 11:14:38 +0300

    lesson 27 done

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import { Route, BrowserRouter } from 'react-router-dom';
 
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/dialogs' component={Dialogs} />
-          <Route path='/profile' component={Profile} />
+          {/* <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} /> */}
+
+          <Route path='/dialogs' render={() => <Dialogs />} />
+          <Route path='/profile' render={() => <Profile />} />
         </div>
       </div>
     </BrowserRouter>
-    )
+  )
 }
 
 export default App;
