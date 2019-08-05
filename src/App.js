58de52d701022e8562118e887fbe91b2cd7351e1
Da1man <chaos4ever@mail.ru>
2019-08-05 22:36:56 +0300

    lesson 31 done

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 
 
 const App = (props) => {
 
   return (
-    <BrowserRouter>
+    
       <div className='app-wrapper'>
         <Header />
         <Navbar state={props.state.sideBar}/>
@@ -25,7 +25,6 @@ const App = (props) => {
             render={() => <Profile state={props.state.profilePage}/>} />
         </div>
       </div>
-    </BrowserRouter>
   )
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,28 +4,26 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-  /*
-  let posts =[
-    {id: 1, post: 'Hi, how are you?', likesCount: 0},
-    {id: 2, post: 'It\'s my first post', likesCount: 26},
-    {id: 2, post: 'bla bla', likesCount: 211},
-    {id: 2, post: 'ae ae', likesCount: 21},
-]
-
-*/
-
   let postsElements = props.posts 
   .map ( p => <Post message={p.post} likesCount ={p.likesCount} /> );
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert (text);
+    
+  }
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-        <textarea></textarea>
+        <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-        <button>Add post</button>
+        <button onClick={ addPost }>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
